9130b667abd2229275bad84cbddad1694eb4939f
Pál Kurgyis <kurgyis.p@gmail.com>
2017-10-16 09:56:29 +0200

    The begining of a beautiful friendship
    dasd

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+//Added
+import { Storage } from '@ionic/storage';
+
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  private url;
+  private name;
+  private gyakList;
+  private defaultImg = "https://d30y9cdsu7xlg0.cloudfront.net/png/883-200.png";
 
-  constructor(public navCtrl: NavController) {
+  constructor(private storage: Storage, public navCtrl: NavController) {
+    this.storage.get('gyakList').then((val) => {
+      if (val == null){
+        this.gyakList = [];
+      }else{
+        this.gyakList = val;
+      }
+    });
+  }
 
+  SaveGyak(){
+    if(this.url == ""){
+      this.url = this.defaultImg;
+    }
+    let temp = {
+      name: this.name,
+      url: this.url
+    };
+
+    this.gyakList.push(temp);
+    this.storage.set('gyakList', this.gyakList);
   }
 
 }
