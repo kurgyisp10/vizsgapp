5bc3a13d2857ff8debae63c6d87dda76a4a2ea25
Dániel Vankó <vankodaniel97@gmail.com>
2017-10-17 21:50:06 +0200

    brightness + barfix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Brightness } from '@ionic-native/brightness';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -11,12 +12,14 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private brightness: Brightness) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      //statusBar.styleDefault();
-      statusBar.backgroundColorByHexString("#25312C");
+      statusBar.styleDefault();
+      statusBar.backgroundColorByHexString("#ffffff");
+      let brightnessValue: number = 0.5;
+      this.brightness.setBrightness(brightnessValue);
       splashScreen.hide();
     });
   }
